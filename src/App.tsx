f5cc096c0a344b65d8ff6d531b9f8b11493dfc83
yanislammari <yanis.lammari937@gmail.com>
2025-08-31 20:58:14 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
